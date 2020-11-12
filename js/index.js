cf30177ddb8bfabc964ace7f521df8492ac53a35
wuyulunbier <997408921@qq.com>
2020-11-12 08:40:40 +0800

    BFC

--- js/index.js
+++ js/index.js
@@ -12,7 +12,38 @@ function sum(num1, num2) {
 const result = sum(20, 30);
 console.log("计算结果:", result);
 
+
+
+console.log("-----------------------");
+
+console.log(a);
+
+var a = 100;
+//变量提前的测试
+
+var b = 200;
+
+console.log(b);
+var scope ="global";
+
+function summer(){
+      console.log(scope);
+     console.log(a);
+}
+summer();
+var fun = function(){
+
+  console.log(b);
+}
+
+ 
+
+
+console.log("-------------------------");
+
 // 3.执行定时器代码
 setTimeout(() => {
   console.log("2s后执行的代码");
 }, 2000);
+
+
